4dab118026acc5ea712b9e5866bce1018e30096d
aloopday <aloopday@gmail.com>
2025-12-07 11:56:47 +0800

    add new to main.jsx, index.html

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {createRoot} from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+/*createRoot(document.getElementById('root')).render(
+  <h1>Hello Charles wong!</h1>
+)*/
+
+const myelement=(
+  <p>Welcome to my world!</p>
+)
+
+createRoot (document.getElementById('demo')).render(
+  myelement
 )
