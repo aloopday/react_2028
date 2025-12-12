10b3c410d2ee5c9ef00e1ffd98242a168ee59a11
aloopday <aloopday@gmail.com>
2025-12-12 11:06:32 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,13 +1,16 @@
-import {createRoot} from 'react-dom/client'
+import {createRoot } from 'react-dom/client'
 
-const myElement=(
-    <>
-    <p>THis is a charles website</p>
-    <p>This is a parents communication platform.</p>
-    
-    </>
-);
+function Car(){
+    return(
+        <>
+        <h2>My Car</h2>
+        <p>It is a Tesla.</p>
+        <blockquote class="twitter-tweet" data-media-max-width="560"><p lang="zh" dir="ltr">范曾得子-_新成语创作者-港村大叔 <a href="https://t.co/Qmy50r7xWr">https://t.co/Qmy50r7xWr</a> via <a href="https://twitter.com/YouTube?ref_src=twsrc%5Etfw">@YouTube</a> <a href="https://t.co/10sqRhxNH0">pic.twitter.com/10sqRhxNH0</a></p>&mdash; 港村大叔爱吹水 Charles (@love98982016) <a href="https://twitter.com/love98982016/status/1999308484643594313?ref_src=twsrc%5Etfw">December 12, 2025</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </>
+    )
+};
 
 createRoot(document.getElementById('root')).render(
-    myElement
-);
+    <Car />
+)
+
