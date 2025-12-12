cdbc177f4d26b949f5121b0ebbe32447bd143b10
aloopday <aloopday@gmail.com>
2025-12-12 17:50:01 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,11 +2,12 @@ import {createRoot} from 'react-dom/client'
 //Check out the index.html file, were we have added 
 
 function Car(){
+    const x ="myclass";
     return(
-        <h1 className="myclass">
-            Hello World
-        </h1>
-    );
+        <>
+        <h1 className={x}>Hello Charles!</h1>
+        </>
+    )
 }
 
 createRoot(document.getElementById('root')).render(
