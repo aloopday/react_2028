05314b838ba50c9d60a37682f6cff537d55fcd64
aloopday <aloopday@gmail.com>
2025-12-12 13:15:42 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,14 +1,14 @@
-import {createRoot} from 'react-dom/client'
+import{createRoot} from 'react-dom/client'
 
 function Car(){
+    const hp=218 *1.36;
     return(
         <>
         <h1>My car</h1>
-        <p>It has {218 *1.36} horsepower</p>
+        <p>It has {hp} horsepower</p>
         </>
     );
 }
-
 createRoot(document.getElementById('root')).render(
     <Car />
-);
+)
