745ea28b4648113ea71148ac5d7d3c9518a4ea6b
charles <charles@charlesdeMac-mini.local>
2025-12-13 22:29:29 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,8 +1,44 @@
-import {createRoot} from 'react-dom/client';
-import Car from './Car.jsx';
+import {createRoot } from 'react-dom/client';
+import React from 'react';
+
+class Car extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            brand:"tesla",
+            model:"model 2",
+            color:"red",
+            year:2025
+        };
+    }
+
+    changeColor = () =>{
+        this.setState({color:"black"});
+    }
+    render(){
+
+        return(
+            <div>
+            <h1>My {this.state.brand}</h1>
+            <p>
+                This is a {this.state.color}
+                {this.state.model}
+                from {this.state.year} .
+            </p>
+            <button
+            type="button"
+            onClick={this.changeColor}>
+
+                Change color
+
+            </button>
+        </div>
+
+        );
+        
+    }
+}
 
 createRoot(document.getElementById('root')).render(
     <Car />
-);
-
-// Make sure to check out the Car.jsx file.
+)
