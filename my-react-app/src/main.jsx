5f5217e647a93271c2d496fd7b914ea28657c00a
aloopday <aloopday@gmail.com>
2025-12-14 11:46:01 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,44 +1,24 @@
-import {createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import React from 'react';
 
-class Car extends React.Component{
+class Header extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            brand:"tesla",
-            model:"model 2",
-            color:"red",
-            year:2025
-        };
+        this.state={favoritecolor:"read"};
     }
-
-    changeColor = () =>{
-        this.setState({color:"black"});
+    componentDidMount(){
+        setTimeout(
+            ()=>{
+                this.setState({favoritecolor:"yellow"}),1000
+            },1000)
     }
     render(){
-
         return(
-            <div>
-            <h1>My {this.state.brand}</h1>
-            <p>
-                This is a {this.state.color}
-                {this.state.model}
-                from {this.state.year} .
-            </p>
-            <button
-            type="button"
-            onClick={this.changeColor}>
-
-                Change color
-
-            </button>
-        </div>
-
+            <h1>My Favorite Color is {this.state.favoritecolor}</h1>
         );
-        
     }
 }
 
 createRoot(document.getElementById('root')).render(
-    <Car />
-)
+    <Header />
+);
