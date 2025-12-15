336798911eb9a3d32a2e53e9b5565518ee80627b
aloopday <aloopday@gmail.com>
2025-12-15 18:18:02 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,46 +1,22 @@
-import{createRoot} from 'react-dom/client'
+import {createRoot} from "react-dom/client"
 
-function Son(props){
-    return(
-        <div style={{background:'lightgreen'}}>
-            <h2>Son</h2>
-            <div>{props.children}</div>
-        </div>
-    );
+function MissedGoal(){
+    return <h1>Missed!!!</h1>;
 }
 
-function Daughter(props){
-    const {brand, model} =props;
-    return(
-        <div style={{background:'lightblue'}}>
-            <h2>Daughter</h2>
-            <div>{props.children}</div>
-        </div>
-    );
+function MadeGoal(){
+    return <h1>Goal!</h1>;
 }
 
-function Parent(){
+function Goal(props){
+    const isGoal =props.isGoal;
     return(
-        <div>
-            <h1>My two Children</h1>
-            <Son>
-                <p>
-                    this  was written in the parents component,
-                    but displayed as a part of the son component
-                </p>
-            </Son>
-            <Daughter>
-                <p>
-                    This was written in the Parent component,
-                    but displayed as a part of the Daughter component.
-                </p>
-            </Daughter>
-            <Son />
-            <Daughter />
-        </div>
+        <>
+        {isGoal?< MadeGoal /> :<MissedGoal />}
+        </>
     );
 }
 
 createRoot(document.getElementById('root')).render(
-    <Parent />
-);
+    <Goal isGoal={true} />
+)
