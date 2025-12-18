c500d8432730a3e02e4986f2eed29a5c71817e91
aloopday <aloopday@gmail.com>
2025-12-18 11:51:18 +0800

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,63 +1,19 @@
+import {createRoot } from 'react-dom/client';
+import {Suspense} from 'react';
+import MyFruits from './Fruits'; // Adjust path as needed
+// click the "Fruits.jsx" tab to check out the Fruits.jsx file
 
-import{useState} from 'react';
-import { createRoot } from 'react-dom/client';
-import{MyApp} from './Modal.jsx';
-import{App} from './Button.jsx';
-
-function MyForm(){
-    const [selectedFruit, setSelectedFruit] =useState('banana');
-
-    const handleChange=(event) =>{
-        setSelectedFruit(event.target.value);
-    };
-
-    const handleSubmit=(event) =>{
-        alert(`Your favorite fruit is :${selectedFruit}`);
-        event.preventDefault();
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <p>Select your favorite fruit:</p>
-            <label>
-                <input 
-                type="radio"
-                name="fruit"
-                value="orange"
-                checked={selectedFruit ==='orange'}
-                onChange={handleChange}
-                
-                /> Orange
-            </label>
-            <br />
-            <label>
-                <input 
-                    type="radio"
-                    name="fruit"
-                    value="apple"
-                    checked={selectedFruit ==='apple'}
-                    onChange={handleChange}
-                />Apple
-            </label>
-            <br />
-            <label>
-                <input 
-                    type="radio"
-                    name="fruit"
-                    value="cherry"
-                    checked ={selectedFruit ==='peach'}
-                    onChange={handleChange}
-                /> Cherry
-                </label>
-                 <br />
-                <button type ="submit">Submit</button>
-                <MyApp />
-                <App />
-        </form>
-        
+function App(){
+    return(
+        <div>
+            <Suspense fallback={<div>Loading ...</div>}>
+            <MyFruits />
+            
+            </Suspense>
+        </div>
     );
 }
 
 createRoot(document.getElementById('root')).render(
-    <MyForm/>
-)
+    <App />
+);
